fdb6c8087ba3a6c5a6778c15bda320e6d97ed8cd
giangvu <khachdoi@gmail.com>
2023-07-22 22:10:00 +0700

    model and dao work item

--- app/src/main/java/com/example/androidnc/database/dao/WorkItemDAO.java
+++ app/src/main/java/com/example/androidnc/database/dao/WorkItemDAO.java
@@ -1,4 +1,35 @@
 package com.example.androidnc.database.dao;
 
-public class WorkItemDAO {
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.Query;
+import android.arch.persistence.room.Update;
+
+import com.example.androidnc.database.model.WorkItem;
+
+import java.util.List;
+
+@Dao
+public interface WorkItemDAO {
+//    @Insert
+//    public void insert(WorkItem item);
+    @Insert
+    public void insert(WorkItem... items);
+    @Update
+    public void update(WorkItem... items);
+    @Delete
+    public void delete(WorkItem item);
+
+    @Query("SELECT * FROM kma_work_item")
+    public List<com.example.androidnc.database.model.WorkItem> getAllWorkItems();
+
+    @Query("SELECT * FROM kma_work_item WHERE work_id = :work_id")
+    public com.example.androidnc.database.model.WorkItem getWorkItemById(String work_id);
+
+    @Query("SELECT * FROM kma_work_item WHERE user_create = :user_create")
+    public com.example.androidnc.database.model.WorkItem getWorkItemByUserCreate(String user_create);
+
+    @Query("SELECT * FROM kma_work_item WHERE user_respond = :user_respond")
+    public com.example.androidnc.database.model.WorkItem getWorkItemByUserRespond(String user_respond);
 }
--- app/src/main/java/com/example/androidnc/database/model/WorkItem.java
+++ app/src/main/java/com/example/androidnc/database/model/WorkItem.java
@@ -4,12 +4,20 @@ import android.arch.persistence.room.Entity;
 import android.arch.persistence.room.PrimaryKey;
 import android.support.annotation.NonNull;
 
+import java.util.Date;
+
 @Entity(tableName = "kma_work_item")
 public class WorkItem {
     @PrimaryKey
     @NonNull  private String work_id;
     private String work_name;
     private String work_description;
-    private Long quantity;
+    private String level_id;
+    private String status_id;
+    private Date work_start;
+    private Date work_end;
+    private String user_create;
+    private String user_respond;
+    private String user_support;
 }
 
