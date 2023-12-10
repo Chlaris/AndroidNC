fc450e1d1208a72755e4321a65a11e04e0cab013
giangvu <khachdoi@gmail.com>
2023-12-10 11:02:10 +0700

    TaskFile = filecache

--- app/src/main/java/com/example/androidnc/database/model/TaskFile.java
+++ app/src/main/java/com/example/androidnc/database/model/TaskFile.java
@@ -12,21 +12,69 @@ public class TaskFile {
     @SerializedName("file_id")
     @Expose
     private Integer file_id;
-    @SerializedName("file_name")
+    @SerializedName("storage")
     @Expose
-    private String file_name;
+    private Integer storage;
     @SerializedName("file_path")
     @Expose
     private String file_path;
+    @SerializedName("hash_path")
+    @Expose
+    private String hash_path;
+    @SerializedName("parent")
+    @Expose
+    private String parent;
+    @SerializedName("file_name")
+    @Expose
+    private String file_name;
+    @SerializedName("mimetype")
+    @Expose
+    private Integer mimetype;
+    @SerializedName("mimepart")
+    @Expose
+    private Integer mimepart;
     @SerializedName("file_size")
     @Expose
     private String file_size;
+    @SerializedName("mtime")
+    @Expose
+    private Integer mtime;
+    @SerializedName("storage_mtime")
+    @Expose
+    private Integer storage_mtime;
+    @SerializedName("encrypted")
+    @Expose
+    private Integer encrypted;
+    @SerializedName("unencrypted_size")
+    @Expose
+    private Integer unencrypted_size;
+    @SerializedName("etag")
+    @Expose
+    private String etag;
+    @SerializedName("permissions")
+    @Expose
+    private Integer permissions;
+    @SerializedName("checksum")
+    @Expose
+    private String checksum;
 
-    public TaskFile(@NonNull Integer file_id, String file_name, String file_path, String file_size) {
+    public TaskFile(@NonNull Integer file_id, Integer storage, String file_path, String hash_path, String parent, String file_name, Integer mimetype, Integer mimepart, String file_size, Integer mtime, Integer storage_mtime, Integer encrypted, Integer unencrypted_size, String etag, Integer permissions, String checksum) {
         this.file_id = file_id;
-        this.file_name = file_name;
+        this.storage = storage;
         this.file_path = file_path;
+        this.hash_path = hash_path;
+        this.parent = parent;
+        this.file_name = file_name;
+        this.mimetype = mimetype;
+        this.mimepart = mimepart;
         this.file_size = file_size;
+        this.mtime = mtime;
+        this.storage_mtime = storage_mtime;
+        this.encrypted = encrypted;
+        this.unencrypted_size = unencrypted_size;
+        this.etag = etag;
+        this.permissions = permissions;
+        this.checksum = checksum;
     }
 
     @NonNull
@@ -34,19 +82,19 @@ public class TaskFile {
         return file_id;
     }
 
-    public String getFile_name() {
-        return file_name;
-    }
-
-    public void setFile_name(String file_name) {
-        this.file_name = file_name;
-    }
-
     public String getFile_path() {
         return file_path;
     }
 
+    public String getFile_name() {
+        return file_name;
+    }
+
     public String getFile_size() {
         return file_size;
+    }
+
+    public void setFile_name(String file_name) {
+        this.file_name = file_name;
     }
 }
